0629398aa43afff04a830948911133ebf225f4aa
yohan-lertourne <yohan.lertourne@gmail.com>
2022-01-21 09:36:57 +0100

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,15 +1,15 @@
 // function
-function fizzBuzz (number) {
+function fizzBuzz(number) {
     let result = "";
     if (number % 3 === 0) {
         result = "fizz";
     }
-    else if (number % 5 === 0){
+    else if (number % 5 === 0) {
         result = "buzz";
     }
 
-    if (number % 3 === 0 && number % 5 === 0){
-        result="fizzbuzz";
+    if (number % 3 === 0 && number % 5 === 0) {
+        result = "fizzbuzz";
     }
     console.log(result);
 }
